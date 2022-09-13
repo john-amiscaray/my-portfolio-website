6a3b85237977cfb84ee5d5a20455127346092a9d
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-09-12 21:49:59 -0400

    More button UI/UX

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const TS = 'Typescript';
 const LP = 'Lisp';
 
 function changeLanguageModalText(language){
-    document.querySelector('#language-info-title').innerText = `How I use ${language}`;
+    $('#language-info-title').text(`How I use ${language}`);
     let languageDes = 'Failed to load language description. Please contact the creator of this site.';
     if(language === KT){
         languageDes = 'Mainly uses this for backend programming in Spring Boot or general programming.';
@@ -16,5 +16,9 @@ function changeLanguageModalText(language){
         languageDes = 'Used this language for a course on data structures that I excelled at. In this course I had to implement' +
             ' and/or work with various data structures using this language.';
     }
-    document.querySelector('#language-modal-info').innerText = languageDes;
+    $('#language-modal-info').text(languageDes);
 }
+
+$('.carousel-item.active').on('click', () => {
+    $('.point-indicator').hide(500);
+});
