46dd5e90ef55997c83749934e6633a718a3be3df
john-amiscaray <john_ernest_amiscaray@outlook.com>
2023-01-23 20:30:35 -0500

    Skeleton

---
+++ main.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0,7,-5);
+
+const sunLight = new THREE.HemisphereLight(0x404040, 0xFFFFFF, 0.5);
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 5;
+camera.position.y = 1.5;
+
+scene.add(pointLight, sunLight);
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    renderer.clearDepth();
+    renderer.render(scene, camera);
+
+}
+
+animate();
